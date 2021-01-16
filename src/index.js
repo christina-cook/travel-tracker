5d508916729e35f8f206ce39e3533d3ef85d1668
Christina Cook <cmc.cook7@gmail.com>
2021-01-16 11:13:29 -0700

    Fetch all data

--- src/index.js
+++ src/index.js
@@ -8,3 +8,33 @@ import './css/base.scss';
 import './images/turing-logo.png'
 
 console.log('This is the JavaScript entry file - your code begins here.');
+
+// import fetchRequests from './fetchRequests';
+
+
+
+const getAllData = () => {
+  let getTravelerData = fetch('http://localhost:3001/api/v1/travelers')
+    .then(response => response.json());
+  let getTripData = fetch('http://localhost:3001/api/v1/trips')
+    .then(response => response.json());
+  let getDestinationData = fetch('http://localhost:3001/api/v1/destinations')
+    .then(response => response.json());
+
+  Promise.all([getTravelerData, getTripData, getDestinationData])
+    .then(response => {
+      const dataset = {};
+      dataset.travelerData = response[0]
+      dataset.tripData = response[1];
+      dataset.destinationData = response[2];
+      console.log(dataset)
+      return dataset;
+    })
+}
+
+const onStartup = () => {
+  getAllData();
+}
+
+
+window.addEventListener('load', onStartup);
