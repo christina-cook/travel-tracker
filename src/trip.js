d607b0d238dc2dd046677a817639f0722dd73996
Christina Cook <cmc.cook7@gmail.com>
2021-01-19 13:35:23 -0700

    Determine trip end date

--- src/trip.js
+++ src/trip.js
@@ -9,6 +9,7 @@ class Trip {
     this.numberOfTravelers = tripData.travelers;
     this.departureDate = tripData.date;
     this.tripDuration = tripData.duration;
+    this.tripEndDate;
     this.status = tripData.status;
     this.suggestedActivities = tripData.suggestedActivities;
     this.destinationInfo = destination;
@@ -16,6 +17,8 @@ class Trip {
   }
 
   calculateTripCost() {
+    this.determineTripEndDate();
+/
     const hotelCost = this.destinationInfo.estimatedLodgingCostPerDay * this.tripDuration;
     const flightCost = this.destinationInfo.estimatedFlightCostPerPerson * this.numberOfTravelers;
     const totalCost = hotelCost + flightCost;
@@ -23,6 +26,15 @@ class Trip {
     this.totalTripCost = totalCost + agentFee;
     return this.totalTripCost;
   }
+
+  determineTripEndDate() {
+    console.log(this.departureDate)
+    const tripStart = moment(this.departureDate).format('YYYY/MM/DD');
+    this.tripEndDate = moment(tripStart).add(this.tripDuration, 'days').format('YYYY/MM/DD');
+    console.log('tripStart', tripStart);
+    console.log('tripEndDate', this.tripEndDate);
+  }
+
 
 }
 
